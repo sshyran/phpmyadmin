8088c8d1922d572cd2095d43ec249c0dfddbc433
Stephen J. Carnam <steve@steveorevo.com>
2017-07-10 19:40:13 -0700

    Support domain button group API

--- src/prepend.php
+++ src/prepend.php
@@ -11,5 +11,14 @@ if ( $ds_runtime->last_ui_event !== false ) return; // Not interested in events
  */
 $ds_runtime->add_action( 'append_tools_menu', 'phpmyadmin_tools_menu' );
 function phpmyadmin_tools_menu() {
-	echo '<li><a href="http://localhost/ds-plugins/phpmyadmin/web/index.php">PHPMyAdmin - MySQL Administration</a></li>';
+  echo '<li><a href="http://localhost/ds-plugins/phpmyadmin/web/index.php">PHPMyAdmin - MySQL Administration</a></li>';
+}
+
+$ds_runtime->add_action( 'domain_button_group', 'phpmyadmin_domain_button_group' );
+function phpmyadmin_domain_button_group($detail) {
+  ?>
+  <a href="http://localhost/ds-plugins/phpmyadmin/web/db_structure.php?db=<?php echo $detail->dbName; ?>" class="btn btn-warning" target="_blank">
+      Database
+  </a>
+  <?php
 }
